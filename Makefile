c166928efdacbf10a75c2a18581f869ef706d882
Gray Watson <gwatson@abinitio.com>
2024-03-20 10:49:10 -0400

    some tweaks

--- Makefile
+++ Makefile
@@ -2,7 +2,13 @@
 # All of the sources are in the src/ subdirectory so this just delegates to the src/Makefile
 #
 
-all : src/Makefile
+all :
+	@ echo "Type 'make configure' to run configure down in the src/ subdirectory."
+	@ echo "Type 'make build' to compile null down in the src/ subdirectory."
+	@ echo "Type 'make test' to run the tests down in the src/ subdirectory."
+	@ echo "Type 'make clean' to clean the compiled stuff."
+
+build : src/Makefile
 	cd src ; make
 
 src/Makefile :
--- src/null_tests.sh
+++ src/null_tests.sh
@@ -9,6 +9,7 @@ set -e
 # -f write file tests
 ##################################################################
 
+echo "Checking write output files..."
 rm -f x.t y.t z.t
 
 cat *.[ch] > x.t
@@ -20,38 +21,49 @@ cmp x.t y.t
 cmp x.t z.t
 
 rm -f x.t y.t z.t
+echo ""
 
 ##################################################################
 # -m md5 signature tests
 ##################################################################
 
+echo "Checking -m md5 argument..."
 # that md5 signature is the empty string
 ./null -m 2>&1 /dev/null | grep "d41d8cd98f00b204e9800998ecf8427e"
+echo ""
 
 ##################################################################
-# -s stop-after signature tests
+# -s stop-after tests
 ##################################################################
 
+echo "Checking stop after -s argument..."
 # should only see output of 100000 bytes
 cat *.[ch] | ./null -v -s 100000 2>&1 | grep 100000
+echo ""
 
 ##################################################################
 # --help and --usage tests
 ##################################################################
 
+echo "Checking --help and --usage..."
 ./null --help 2>&1 | grep "Null Utility:"
 ./null --usage 2>&1 | grep -- --help
+echo ""
 
 ##################################################################
 # -t throttle and rate tests
 ##################################################################
 
+echo "Checking throttle and rate tests..."
 # should take more than a second because throttling and should see rate info
 cat *.[ch] | ./null -b 10k -s 100k -t 50k -R 1 2>&1 | grep "Writing at"
+echo ""
 
 ##################################################################
 # -r and -w read and write pagination tests
 ##################################################################
 
+echo "Checking read and write pagination..."
 # should not get error because tar worked
 tar -czf - . | ./null -w -p | ./null -rpv | tar -tzvf - > /dev/null
+echo ""
